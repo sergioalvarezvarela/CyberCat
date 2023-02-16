76e2fa7957b586c7dcbb42ed9f1e5499fbb3b4cb
Sergio Alvarez <sergio.alvarez.varela@udc.es>
2023-02-16 15:43:28 +0100

    [USER] Impl. de create user y save profile image en service y repository

--- src/main/java/es/cybercatapp/business/services/UserService.java
+++ src/main/java/es/cybercatapp/business/services/UserService.java
@@ -2,20 +2,28 @@ package es.cybercatapp.business.services;
 
 import es.cybercatapp.business.entities.User;
 import es.cybercatapp.business.exceptions.DuplicatedResourceException;
+import es.cybercatapp.business.repositories.UserRepository;
 import es.cybercatapp.business.utils.ExceptionGenerationUtils;
 import es.cybercatapp.common.ConfigurationParameters;
 import es.cybercatapp.common.Constants;
+import org.apache.commons.io.IOUtils;
 import org.mindrot.jbcrypt.BCrypt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.time.LocalDateTime;
 
 @Service
 public class UserService {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
     private static final String SALT = "$2a$10$MN0gK0ldpCgN9jx6r0VYQO";
     @Autowired
     ConfigurationParameters configurationParameters;
@@ -23,13 +31,16 @@ public class UserService {
     @Autowired
     ExceptionGenerationUtils exceptionGenerationUtils;
 
+    @Autowired
+    private UserRepository userRepository;
+
     private File resourcesDir;
 
     @PostConstruct
     public void init() {
         resourcesDir = new File(configurationParameters.getResources());
     }
-   /* @Transactional
+    @Transactional
     public User create(String username, String email, String password, String address,
                        String image, byte[] imageContents) throws DuplicatedResourceException {
         if (userRepository.findByEmail(email) != null) {
@@ -43,5 +54,18 @@ public class UserService {
         User user = userRepository.create(new User(username, email, BCrypt.hashpw(password, SALT), false, LocalDateTime.now(),image));
         saveProfileImage(user.getUserId(), image, imageContents);
         return user;
-    }*/
+    }
+    private void saveProfileImage(Long id, String image, byte[] imageContents) {
+        if (image != null && image.trim().length() > 0 && imageContents != null) {
+            File userDir = new File(resourcesDir, id.toString());
+            userDir.mkdirs();
+            File profilePicture = new File(userDir, image);
+            try (FileOutputStream outputStream = new FileOutputStream(profilePicture);) {
+                IOUtils.copy(new ByteArrayInputStream(imageContents), outputStream);
+            } catch (Exception e) {
+                logger.error(e.getMessage(), e);
+            }
+        }
+    }
+
 }
